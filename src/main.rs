ac954c1780808bee2f8597411f6a3ad01f6944c2
oreo3494 <nileshkumar.newdelhi@gmail.com>
2024-11-09 22:41:45 +0530

    added next screen

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ slint::include_modules!();
 fn main() {
     let window = AppWindow::new().unwrap();
     let pc_pick = Rc::new(RefCell::new(1));
+    let matches_left = Rc::new(RefCell::new(0));
 
     let picked = window.as_weak();
     let pc_clone = Rc::clone(&pc_pick); // Rc clone of `pc_pick`
@@ -16,6 +17,17 @@ fn main() {
         *pc_pick = 2;
 
         println!("You picked {weapon}!! i see ya :D");
+    });
+
+
+    let matched = window.as_weak();
+    let match_clone = Rc::clone(&matches_left);
+    window.on_match_selected(move |matches| {
+        let app = matched.upgrade().unwrap();
+        *match_clone.borrow_mut() = matches;
+        app.set_next_round_opacity(0.0);
+        app.set_start_passive(true);
+        println!("match {matches}");
     });
 
     window.run().unwrap();
