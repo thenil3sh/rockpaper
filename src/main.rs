082b3fc6c322037f09f71da4b7147f11e27b1065
oreo3494 <nileshkumar.newdelhi@gmail.com>
2024-11-15 22:18:30 +0530

    a loads of changes

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,64 @@ use rand::{seq::SliceRandom, thread_rng};
 slint::include_modules!();
 fn main() {
     let window = AppWindow::new().unwrap();
-    let pc_pick = Rc::new(RefCell::new(1));
     let matches_left = Rc::new(RefCell::new(0));
+    let pc_score = Rc::new(RefCell::new(0));
+    let yt_score = Rc::new(RefCell::new(0));
+    let win_count = Rc::new(RefCell::new(0));
 
-    let picked = window.as_weak();
-    let pc_clone = Rc::clone(&pc_pick); // Rc clone of `pc_pick`
-
+    let matches_clone = Rc::clone(&matches_left);
+    let picked = window.as_weak(); // Rc clone of `pc_pick`
+    let yr_clone = Rc::clone(&yt_score);
+    let pc_clone = Rc::clone(&pc_score);
+    let win_clone = Rc::clone(&win_count);
     window.on_picked(move |weapon| {
-        let mut pc_pick = pc_clone.borrow_mut();
-        *pc_pick = 2;
+        let mut yr_score = yr_clone.borrow_mut();
+        let mut pc_score = pc_clone.borrow_mut();
+        let app = picked.upgrade().unwrap();
+        let pc_pick = *[0, 1, 2].choose(&mut rand::thread_rng()).unwrap();
+        app.set_pc_pick(pc_pick);
+        app.set_yr_pick( weapon );
+        app.set_winning(
+        if pc_pick == 0 && weapon == 1 {
+            *yr_score += 1;
+            0
+        } else if pc_pick == 1 && weapon == 2 {
+            *yr_score += 1;
+            0
+        } else if pc_pick == 2 && weapon == 0 {
+            *yr_score += 1;
+            0
+        } else if pc_pick == 1 && weapon == 0 {
+            *pc_score += 1;
+            1
+        } else if pc_pick == 2 && weapon == 1 {
+            *pc_score += 1;
+            1
+        } else if pc_pick == 0 && weapon == 2 {
+            *pc_score += 1;
+            1
+        } else if pc_pick == weapon {
+            *pc_score += 1;
+            *yr_score += 1;
+            match weapon {
+                x => x + 2,
+            }
+        } else {
+            panic!("Again whatt??");
+        });
+        app.set_pc_score(*pc_score);
+        app.set_yr_score(*yr_score);
+        *win_clone.borrow_mut() = if *yr_score > *pc_score {
+            *yr_score
+        } else {
+            *pc_score
+        };
 
-        println!("You picked {weapon}!! i see ya :D");
+        if *matches_clone.borrow() == *win_clone.borrow() {
+            todo!("lmao ok");
+        }
+
+        println!("You picked {}!! i see ya :D and i picked {}", pc_pick, weapon);
     });
 
 
