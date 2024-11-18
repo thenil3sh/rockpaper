5a48202ce8b5cb93f33db3b7e235e99efaf56bc3
oreo3494 <107793551+oreo3494@users.noreply.github.com>
2024-11-18 22:05:26 +0530

    almost there main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-#![windows_subsystem = "windows"]
-use slint;
+use rand::rngs::ThreadRng;
+use slint::{self, SharedString};
 use std::cell::RefCell; 
 use std::rc::Rc;
 
-use rand::{seq::SliceRandom, thread_rng};
+use rand::seq::SliceRandom;
 slint::include_modules!();
+
+
+const WIN_TEXT : [&str; 4] = ["+1 goes to 'you'","PC lost it", "+1!!!", "+0 goes to 'PC'"];
+const LOSE_TEXT : [&str;5] = ["PC gets +1", "+1 goes to 'PC'", "0 goes to 'you'", "+0!!!", "0_0"];
+const DRAW_TEXT : [&str;5] = ["both get +1", "draw!", "your hand = pc hand", "🤝", "draw"];
+
 
 fn main() {
     let window = AppWindow::new().unwrap();
@@ -26,27 +32,18 @@ fn main() {
         app.set_pc_pick(pc_pick);
         app.set_yr_pick( weapon );
         app.set_winning(
-        if pc_pick == 0 && weapon == 1 {
+        if pc_pick == 0 && weapon == 1 || pc_pick == 1 && weapon == 2 || pc_pick == 2 && weapon == 0 {
             *yr_score += 1;
+            app.set_result_text(SharedString::from(*WIN_TEXT.choose(&mut rand::thread_rng()).unwrap()));
             0
-        } else if pc_pick == 1 && weapon == 2 {
-            *yr_score += 1;
-            0
-        } else if pc_pick == 2 && weapon == 0 {
-            *yr_score += 1;
-            0
-        } else if pc_pick == 1 && weapon == 0 {
+        } else if pc_pick == 1 && weapon == 0 || pc_pick == 2 && weapon == 1 || pc_pick == 0 && weapon == 2 {
             *pc_score += 1;
-            1
-        } else if pc_pick == 2 && weapon == 1 {
-            *pc_score += 1;
-            1
-        } else if pc_pick == 0 && weapon == 2 {
-            *pc_score += 1;
+            app.set_result_text(SharedString::from(*LOSE_TEXT.choose(&mut rand::thread_rng()).unwrap()));
             1
         } else if pc_pick == weapon {
             *pc_score += 1;
             *yr_score += 1;
+            app.set_result_text(SharedString::from(*DRAW_TEXT.choose(&mut rand::thread_rng()).unwrap()));
             match weapon {
                 x => x + 2,
             }
@@ -62,7 +59,18 @@ fn main() {
         };
 
         if *matches_clone.borrow() == *win_clone.borrow() {
-            panic!("dnag it");
+            app.set_game_over(true);
+            app.set_win(
+                if *yr_score > *pc_score {
+                    1
+                } else if *pc_score > *yr_score {
+                    app.set_pcString(SharedString::from("Sad :("));
+                    0
+                } else {
+                    app.set_pcString(SharedString::from("it's a draw"));
+                    2
+                }
+            )
         }
 
         println!("You picked {}!! i see ya :D and i picked {}", pc_pick, weapon);
@@ -80,5 +88,4 @@ fn main() {
     });
 
     window.run().unwrap();
-    println!("Hello, world!");
 }
